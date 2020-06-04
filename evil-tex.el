4930aae0776153617406e0c26395ce09dd55e328
yoavm448 <yoavm448@gmail.com>
2020-06-04 15:49:15 +0300

    Revert "dolist + cons unwrapping -> cl-loop"
    This reverts commit c72dcd5f3c0b6553d0d26e26ac230367a98da824.

--- evil-tex.el
+++ evil-tex.el
@@ -639,8 +639,10 @@ PREFIX is the prefix to give the generated functions created
 by (lambda () (interactive) (SINGLE-STRINGS-FN env)) if the input is a string,
 and by (lambda () (interactive) (CONS-FN env)) if it's a cons
 Return KEYMAP."
-  (cl-loop for (key . env) in generator-alist do
-    (let (name)
+  (dolist (pair generator-alist)
+    (let ((key (car pair))
+          (env (cdr pair))
+          name)
       (cond
        ((stringp env)
         (setq name (intern (concat prefix env)))
