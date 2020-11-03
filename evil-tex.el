ac313efb22d621c093d8d30233bd7dc8b4cc54b4
Yoav Marco <yoavm448@gmail.com>
2020-11-03 16:10:31 +0200

    Bump version

--- evil-tex.el
+++ evil-tex.el
@@ -6,7 +6,7 @@
 ;; Maintainers: Yoav Marco <yoavm448@gmail.com>, Itai Y. Efrat <itai3397@gmail.com>
 ;; Created: February 01, 2020
 ;; Modified: August 02, 2020
-;; Version: 1.0.1
+;; Version: 1.0.2
 ;; Keywords: tex, emulation, vi, evil, wp
 ;; Homepage: https://github.com/iyefrat/evil-tex
 ;; Package-Requires: ((emacs "25.1") (evil "1.0") (auctex "11.88"))
